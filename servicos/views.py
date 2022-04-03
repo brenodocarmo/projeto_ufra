ba2e7dbc808a8382fbbaaa2880ae59dc82c22e66
Breno do Carmo Sousa <brenocarmosousa@gmail.com>
2022-04-02 22:50:57 -0300

    Segunda Fase

--- servicos/views.py
+++ servicos/views.py
@@ -1,7 +1,7 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 
 
 def index(request):
-    return HttpResponse("Deu Certo Breno")
+    return render(request,'index.html')
