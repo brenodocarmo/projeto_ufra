0d75dcb4c065c5c6cb2143a41d6b203651671aa1
Breno do Carmo Sousa <brenocarmosousa@gmail.com>
2022-04-05 01:03:15 -0300

    Alteração no icone de ações

--- controleprojeto/settings.py
+++ controleprojeto/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'servicos', # Nome do APP
+    'fontawesomefree',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
