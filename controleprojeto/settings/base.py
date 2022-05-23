8f0dc0cf92d969be8d30b3a8c206fda5c4bcb344
David S. Gonçalves <davidsilvapg13@gmail.com>
2022-05-23 19:29:28 -0300

    Static

--- controleprojeto/settings/base.py
+++ controleprojeto/settings/base.py
@@ -134,12 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'controleprojeto/static')
-]
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'controleprojeto/static')
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
