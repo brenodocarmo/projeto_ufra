f1d5ad55998a37c9ec01eeda5f1c59c4a90b4d86
David S. Gonçalves <davidsilvapg13@gmail.com>
2022-05-26 13:33:20 -0300

    Teste Whitenoise

--- controleprojeto/settings/heroku.py
+++ controleprojeto/settings/heroku.py
@@ -4,8 +4,8 @@ from controleprojeto.settings.base import *
 
 env = environ.Env()
 
-#DEBUG = env.bool("DEBUG", False)
-DEBUG = env.bool(True)
+DEBUG = env.bool("DEBUG", False)
+
 SECRET_KEY = env("SECRET_KEY")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
