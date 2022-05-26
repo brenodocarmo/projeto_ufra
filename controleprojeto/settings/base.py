adc29204450f93c2c8748585c5696bd92c98c5db
David S. Gonçalves <davidsilvapg13@gmail.com>
2022-05-26 18:20:47 -0300

    Teste Whitenoise

--- controleprojeto/settings/base.py
+++ controleprojeto/settings/base.py
@@ -146,7 +146,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Default primary key field type
